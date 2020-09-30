3fc40f11ed213e018706e249d7b13ef8872c927d
cristianJourdan <2106549@talnet.nl>
2020-09-30 12:16:55 +0200

    forms taak 2

---
+++ 3-Forms/taak02/gegevens.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include "index.html";?>
+    <title>Document</title>
+</head>
+<body>
+    <P>welcome      <?php echo $_GET["fname"];?><br>
+       achternaam:  <?php echo $_GET["lname"];?><br>
+       klas:        <?php echo $_GET["klas"];?><br>
+       leeftijd:    <?php echo $_GET["leeftijd"];?><br>
+       adres:       <?php echo $_GET["adres"];?><br>
+       plaats:      <?php echo $_GET["plaats"];?><br>
+       favomuziek:  <?php echo $_GET["favomu"];?><br></p>
+</body>
+</html>
---
+++ 4-Functions/taak01/functies-1.php
@@ -0,0 +1,12 @@
+<?php
+function schrijfkleur($skleur){
+
+    echo "Deze tekst is in de kleur $skleur <br>";
+    
+}
+    schrijfkleur("rood");
+    schrijfkleur("blauw");
+    schrijfkleur("groen");
+    schrijfkleur("geel");
+
+?>
